bb859b929c5664a487806e8f10f7639ea7b25ecb
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-02-06 15:46:14 -0330

    Add reboot prompt

--- migrate.sh
+++ migrate.sh
@@ -17,20 +17,30 @@ fi
 
 # Only run if on Kinoite or Silverblue
 if [ "${OS}" = "Fedora Linux" ] && [ "${VAR}" = "Kinoite" ]; then
-    echo "System is running Fedora Kinoite. Ready to rebase to: ${IMG_NAME}."
+    echo "System is running Fedora Kinoite. Ready to migrate to: ${IMG_NAME}."
 elif [ "${OS}" = "Fedora Linux" ] && [ "${VAR}" = "Silverblue" ]; then
-    echo "System is running Fedora Silverblue. Ready to rebase to: ${IMG_NAME}."
+    echo "System is running Fedora Silverblue. Ready to migrate to: ${IMG_NAME}."
 else 
     echo "Host system is neither Fedora Silverblue or Fedora Kinoite. This script only supports migrating from these two systems. Please install Fedora Silverblue or Fedora Kinoite and run the script again."
     exit 1
 fi
 
-read -r -p "WARNING: Rebasing to ${IMG_NAME} will delete all preinstalled flatpaks. This script is meant to only be used on a new Kinoite or Silverblue install. Do you wish to continue? [y/N]" response
-if [[ ! $response =~ ^[Yy]$ ]]; then
+read -r -p "WARNING: Rebasing to ${IMG_NAME} will delete all preinstalled flatpaks. This script is meant to only be used on a new Kinoite or Silverblue install. Do you wish to continue? [y/N]" migrationresponse
+if [[ ! $migrationresponse =~ ^[Yy]$ ]]; then
     echo "Confirmation not recieved. System will not be migrated."
     exit 1
 fi
 
 echo "Migrating to ${IMG_NAME}. Running rpm-ostree rebase && setting the kernel arguments."
 
-pkexec sh -c "rpm-ostree rebase --experimental ${IMG}; rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 --append=nvidia.NVreg_DynamicPowerManagement=0x02 --append=nvidia.NVreg_DynamicPowerManagementVideoMemoryThreshold=200; systemctl reboot"
+pkexec sh -c "rpm-ostree rebase --experimental ${IMG}; rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 --append=nvidia.NVreg_DynamicPowerManagement=0x02 --append=nvidia.NVreg_DynamicPowerManagementVideoMemoryThreshold=200"
+
+echo "System migrated. Reboot to run the new image."
+
+read -r -p "Do you wish to reboot now? [y/N]" rebootresponse
+
+if [[ ! $rebootresponse =~ ^[Yy]$ ]]; then
+    echo "Rebooting in 5 seconds..."
+    sleep 5
+    systemctl reboot
+fi
